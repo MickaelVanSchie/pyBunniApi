a4d00a89c446903975ab8aea59b100ee598eb8fa
Mickael <mickael@sparkles.io>
2023-08-10 11:13:19 +0200

    added tests for all objects.

--- tests/test_initiate_models.py
+++ tests/test_initiate_models.py
@@ -1,7 +1,11 @@
+from ..pyBunniApi.objects.time import Duration, TimeObject
+from ..pyBunniApi.objects.invoice import Invoice
+from ..pyBunniApi.objects.row import Row
+from ..pyBunniApi.objects.project import Project
 from ..pyBunniApi.objects.contact import Contact
 
 
-def test_initiate_contact():
+def test_contact_object():
     company_name = 'Some Company Name'
     attn = 'Berry the Bunny'
     street = 'Carrot street'
@@ -20,3 +24,75 @@ def test_initiate_contact():
     )
 
     assert contact
+
+
+def test_row_object():
+    row = Row(
+        unit_price=10.5,
+        description='this is a test description',
+        quantity=5,
+        tax='NL_High21',
+    )
+    assert row
+
+
+def test_invoice_object():
+    contact = Contact(
+        company_name='Some Company Name',
+        attn='Berry the Bunny',
+        street='Carrot street',
+        street_number='21',
+        postal_code='1234AB',
+        city='Carrot Town',
+        phone_number='123456',
+    )
+    row = Row(
+        unit_price=10.5,
+        description='this is a test description',
+        quantity=5,
+        tax='NL_High21',
+
+    )
+    invoice = Invoice(
+        invoice_date='2023-08-01',
+        invoice_number='12345.67',
+        tax_mode='NL_High_21',
+        design='de_XXXX',
+        contact=contact,
+        rows=[row],
+    )
+
+    assert invoice
+
+
+def test_project_object():
+    project = Project(
+        color='#123456',
+        external_id='EXTERNAL ID',
+        id='INTERNAL ID',
+        name='Berry the Bunny'
+    )
+    assert project
+
+
+def test_time_object():
+    duration = Duration(
+        h=2,
+        m=30,
+    )
+    project = Project(
+        color='#123456',
+        external_id='EXTERNAL ID',
+        id='INTERNAL ID',
+        name='Berry the Bunny'
+    )
+    time_object = TimeObject(
+        id='tr_XXXX',
+        date='2023-08-01',
+        duration=duration,
+        description='some description string here',
+        external_id='external id',
+        project=project,
+    )
+
+    assert time_object
