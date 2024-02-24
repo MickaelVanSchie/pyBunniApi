55f4acbbdcaebe35a38bc97a7566579b47266d75
Mickael van Schie <mickael.v.s.19@hotmail.com>
2024-02-24 16:21:43 +0100

    Made properties of InvoiceDesign optional. Most functions in bunni don't use those.

--- pyBunniApi/objects/invoicedesign.py
+++ pyBunniApi/objects/invoicedesign.py
@@ -1,5 +1,5 @@
 class InvoiceDesign:
-    def __init__(self, id: str, name: str, createdOn: str):
+    def __init__(self, id: str, name: str | None = None, createdOn: str | None = None):
         self.id = id
         self.name = name
         self.created_on = createdOn
@@ -7,5 +7,3 @@ class InvoiceDesign:
     id: str
     name: str
     created_on: str  # Todo: Make this a proper date
-
-
