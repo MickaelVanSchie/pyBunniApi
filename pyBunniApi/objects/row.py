c4d7f7187a6259a707c2946da429350d53198be8
Mickael van Schie <mickael.v.s.19@hotmail.com>
2024-02-25 20:10:33 +0100

    row object now works like it should

--- pyBunniApi/objects/row.py
+++ pyBunniApi/objects/row.py
@@ -1,6 +1,6 @@
 import json
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Mapping, Any
 
 from pyBunniApi.objects.category import Category
 from pyBunniApi.tools.case_convert import to_snake_case
@@ -19,8 +19,24 @@ class Row:
 
     def __init__(
             self,
-            **kwargs: Optional[dict]
+            description: Optional[str] = None,
+            quantity: Optional[float] = None,
+            tax: Optional[str] = None,
+            unit_price: Optional[float] = None,
+            booking_category: Optional[Category] = None,
+            **kwargs: Mapping[Any, Any]
     ) -> None:
+        # For init via pyBunniApi
+        if description:
+            self.description = description
+        if quantity:
+            self.quantity = quantity
+        if tax:
+            self.tax = tax
+        if unit_price:
+            self.unit_price = unit_price
+        if booking_category:
+            self.booking_category = booking_category
         for key, value in kwargs.items():
             setattr(self, to_snake_case(key), value)
 
