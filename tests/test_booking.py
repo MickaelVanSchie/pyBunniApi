1c94c893ecee5f9d83903ac886b3c89934d7a771
Mickael van Schie <mickael.v.s.19@hotmail.com>
2025-04-24 13:38:55 +0200

    I should have seen this.

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -7,13 +7,13 @@ def test_booking_list(testClient: Client, booking: dict):
     testClient.create_http_request.return_value = {"items": [booking, booking]}
     resp = testClient.bookings.list()
 
-    assert resp == [booking, booking]
-    assert type(resp[0]) == Booking
+    assert isinstance(resp[0], Booking)
+    assert len(resp) == 2
 
 
 def test_booking_list_untyped(untypedClient: Client, booking: dict):
     untypedClient.create_http_request.return_value = {"items": [booking, booking]}
     resp = untypedClient.bookings.untyped_list()
 
-    assert resp == [booking, booking]
-    assert type(resp[0]) == dict
+    assert isinstance(resp[0], dict)
+    assert len(resp) == 2
