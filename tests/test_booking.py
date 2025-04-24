14f772c27d5c612b72c0f347f8e77b258e0234d9
Mickael van Schie <mickael.v.s.19@hotmail.com>
2025-04-24 13:20:05 +0200

    Added tests.

---
+++ tests/test_booking.py
@@ -0,0 +1,22 @@
+from pyBunniApi.client import Client
+
+from pyBunniApi.objects.booking import Booking
+
+
+def test_booking_list(testClient: Client, booking: dict):
+    booking = Booking(**booking)
+    testClient.create_http_request.return_value = [booking, booking]
+
+    resp = testClient.booking.list()
+
+    assert resp == [booking, booking]
+    assert type(resp[0]) == Booking
+
+
+def test_booking_list_untyped(untypedClient: Client, booking: dict):
+    booking = Booking(**booking)
+    untypedClient.create_http_request.return_value = [booking, booking]
+    resp = untypedClient.bookings.untyped_list()
+
+    assert resp == [booking, booking]
+    assert type(resp[0]) == dict
