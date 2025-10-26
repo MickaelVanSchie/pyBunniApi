fa39775ec3105745312e4cf42db25236177a13bf
unknown <mickael.v.s.19@hotmail.com>
2025-10-26 12:03:48 +0100

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyBunniApi',
-    version='1.2.6',
+    version='1.2.7',
     description='A API Client for communicating with the Bunni accounting software',
     url='https://github.com/sme4gle/pyBunniApi',
     author='Mickael van Schie',
